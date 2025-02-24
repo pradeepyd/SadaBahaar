4e6f164555ef6435378ac10007e739d1bcc90294
Pradeep yadav <pradeep20020102@gmail.com>
2025-02-24 20:08:04 +0545

    user can add streams route created

--- src/app/api/streams/route.ts
+++ src/app/api/streams/route.ts
@@ -1,5 +1,10 @@
-import { NextRequest } from "next/server";
+import prisma from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod';
+
+const YT_REGEX = new RegExp("^https?:\/\/(www\.)?youtube\.com\/watch\?v=[\w-]+$") 
+const SPOTIFY_REGEX = new RegExp("^https?:\/\/open\.spotify\.com\/track\/[\w-]+$")
+
 
 const CreateStreamSchema = z.object({
     creatorId : z.string(),
@@ -9,9 +14,31 @@ const CreateStreamSchema = z.object({
 
 export async function POST( req : NextRequest){
     try{
-        const data = CreateStreamSchema.safeParse(await req.json())
+        const data = CreateStreamSchema.parse(await req.json())
+        const isYT = YT_REGEX.test(data.url);
+        if(!isYT){
+            return  NextResponse.json({
+                message : "Wrong Url "
+            },{
+                status:411
+            })
+        }
+        
+        const extractedId = data.url.split("?v=")[1]
 
+        prisma.stream.create({
+            data:{
+                userId:data.creatorId,
+                url:data.url,
+                extractedId,
+                type:"Youtube"
+            }  
+        })
     }catch(e){
-
+        return  NextResponse.json({
+            message : "Error while adding streams"
+        },{
+            status:411
+        })
     }
 }
