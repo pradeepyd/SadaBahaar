1242dc32c03d0f5054ebbf5a5b441c5fdaad7930
Pradeep yadav <pradeep20020102@gmail.com>
2025-02-28 23:19:16 +0545

    all components done in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
-import { Appbar } from "@/components/Appbar";
-import Image from "next/image";
+import type React from "react"
+
+import Link from "next/link"
+import { Music, Users, ThumbsUp, ChevronRight, Share2, Sparkles } from "lucide-react"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { SiteHeader } from "@/components/site-header"
+import Hero from "@/components/Herosection"
+import FeatureSection from "@/components/FeatureSection"
+import HowItWorks from "@/components/Howitworks"
+import { CTAsection } from "@/components/CTAsection"
+import { FooterSection } from "@/components/FooterSection"
 
 export default function Home() {
   return (
-    <div>
-      <Appbar/>
+    <div className="min-h-screen bg-background flex flex-col">
+      <SiteHeader />
+
+      <main className="flex-grow">
+        <Hero/>
+        <FeatureSection/>
+        <HowItWorks/>
+        <CTAsection/>
+      </main>
+      <FooterSection/>
     </div>
-  );
+  )
 }
+
+
+
+
+
+
+
