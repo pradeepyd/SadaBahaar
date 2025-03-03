85c7cd9bd771f98349419d050e5e1bfa49f00eac
Pradeep yadav <pradeep20020102@gmail.com>
2025-03-03 22:53:20 +0545

    final dashboard done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,16 +10,13 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <SiteHeader />
-      <div className="flex-grow container grid grid-cols-1 md:grid-cols-4 gap-6 py-8">
-        <Sidebar className="hidden md:block" />
+      <div className="flex-grow container grid grid-cols-1 md:grid-cols-4 gap-6 py-8 s">
+        <Sidebar className="w-64 hidden md:block" />
         <main className="col-span-1 md:col-span-3 space-y-6">
           <div className="flex items-center justify-between">
             <h1 className="text-3xl font-bold tracking-tight">Trending Streams</h1>
-            <AddStreamButton />
           </div>
-          <MusicStreamList />
           <CreatorDashboard/>
-          <ExpandableCardDemo/>
         </main>
       </div>
       <FooterSection />
