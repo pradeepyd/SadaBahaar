188b9446e490f93465f9e9f0e020a332f2c5af00
Pradeep yadav <pradeep20020102@gmail.com>
2025-06-06 22:43:04 +0545

    public routes added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,17 @@
 import { clerkMiddleware , createRouteMatcher} from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)','/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(["/",'/sign-in(.*)','/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
+  const user = auth();
+  const userId = (await user).userId;
+  const url = new URL(req.url);
+
+  if(userId && isPublicRoute(req) && url.pathname !== "/"){
+    return NextResponse.redirect(new URL("/dashboard",req.url))
+  }
+  
   if (!isPublicRoute(req)) {
     await auth.protect()
   }
