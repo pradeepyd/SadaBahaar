4f7851044584156f7fc4d6fcd1abf468786731aa
Pradeep yadav <pradeep20020102@gmail.com>
2025-06-07 23:18:47 +0545

    common component for all card

--- src/components/CardWrapper.tsx
+++ src/components/CardWrapper.tsx
@@ -11,6 +11,7 @@ interface CardWrapperProps{
     footerDescription:string;
     backButtonHref: string ;
     backButtonLabel:string;
+    backButtonDisabled?: boolean;
     onClick?:()=> void;
 };
 
@@ -21,12 +22,13 @@ export const CardWrapper = ({
     footerDescription,
     backButtonLabel,
     backButtonHref,
+    backButtonDisabled,
     onClick
 }:CardWrapperProps) => {
     return(
         <Card className="w-full max-w-md mx-auto bg-black/40 border-gray-800">
-            <CardHeader className="text-center pb-6">
-                <CardTitle className="text-2xl font-bold text-white mb-3">
+            <CardHeader className="text-center pb-3">
+                <CardTitle className="text-2xl font-bold text-white mb-2">
                     {titleLabel}
                 </CardTitle>
                 <CardDescription className="text-gray-400 text-base">
@@ -34,12 +36,12 @@ export const CardWrapper = ({
                 </CardDescription>
             </CardHeader>
             <Separator className="bg-gray-800" />
-            <CardContent className="space-y-6 pt-6 px-8 pb-8">
+            <CardContent className="space-y-6 pt-6 px-8 pb-5">
                 {children}
             </CardContent>    
                 <CardFooter className="px-8 flex items-center justify-center gap-2 text-sm text-gray-400 pt-2">   
                     <span>{footerDescription}</span>
-                    <BackButton href={backButtonHref} label={backButtonLabel} onClick={onClick}>
+                    <BackButton href={backButtonHref} label={backButtonLabel} onClick={onClick} disabled={backButtonDisabled}>
                     </BackButton>
                 </CardFooter>
         </Card>
