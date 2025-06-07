a298f721afc902a58c93dac960a2aeda4f9b4e58
Pradeep yadav <pradeep20020102@gmail.com>
2025-06-07 23:17:52 +0545

    error form created

--- src/components/auth/formError.tsx
+++ src/components/auth/formError.tsx
@@ -2,7 +2,7 @@
 import { BsExclamationTriangle } from "react-icons/bs";
 
 interface FormErrorProps{
-    message?:string;
+    message?:string | undefined | null;
 }
 
 export const FormError = ({message}:FormErrorProps) => {
@@ -12,7 +12,7 @@ export const FormError = ({message}:FormErrorProps) => {
 
     if(message){
         return (
-            <div className="bg-destructive/15 p-2.5 rounded-md flex items-center gap-x-2 text-sm text-destructive">
+            <div className="bg-destructive/15 p-2.5 rounded-md flex items-center gap-x-2 text-sm text-destructive text-red-400 ">
                 <BsExclamationTriangle className="h-4 w-4"/>
                 <p>{message}</p>
             </div>
